c0bdf064f35b4b900752f2ca6f5e45f1873af33f
mstarrk <deddomanwandarlando@gmail.com>
2021-07-22 21:01:05 +0400

    SSL requirement

--- controllers/Database.js
+++ controllers/Database.js
@@ -8,7 +8,13 @@ const Sequelize = require('sequelize');
 console.log('ENV DB details: '+ process.env.DATABASE_URL);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialect: 'postgres'
+	dialect: 'postgres',
+	dialectOptions: {
+		ssl: {
+		  require: true,
+		  rejectUnauthorized: false
+	  }
+	}
 });
 
 module.exports = sequelize;
