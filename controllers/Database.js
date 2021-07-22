49c361764e95a97c40a62174f7bea9640be0091d
mstarrk <deddomanwandarlando@gmail.com>
2021-07-22 20:16:30 +0400

    Production env vars

--- controllers/Database.js
+++ controllers/Database.js
@@ -7,10 +7,10 @@ const Sequelize = require('sequelize');
 
 // DB
 
-const host = process.env.DB_HOST_DEV;
-const user = process.env.DB_USER_DEV;
-const pwd = process.env.DB_PASSWORD_DEV;
-const dbName = process.env.DB_NAME_DEV;
+const host = process.env.DB_HOST_PROD;
+const user = process.env.DB_USER_PROD;
+const pwd = process.env.DB_PASSWORD_PROD;
+const dbName = process.env.DB_NAME_PROD;
 
 const sequelize = new Sequelize(dbName, user, pwd, {
 	host,
