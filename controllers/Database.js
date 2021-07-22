c4084532a757a2d7c0890e34c61ddf3bd76ee00b
mstarrk <deddomanwandarlando@gmail.com>
2021-07-22 20:49:58 +0400

    Update Database.js
    using Heroku env vars.

--- controllers/Database.js
+++ controllers/Database.js
@@ -5,18 +5,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-// DB
-
-const host = process.env.DB_HOST_PROD;
-const user = process.env.DB_USER_PROD;
-const pwd = process.env.DB_PASSWORD_PROD;
-const dbName = process.env.DB_NAME_PROD;
-
-const sequelize = new Sequelize(dbName, user, pwd, {
-	host,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres'
 });
-
-console.log('Sequelize object info: ' + sequelize);
 
 module.exports = sequelize;
